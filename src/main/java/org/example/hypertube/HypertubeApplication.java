2248e51651ece4d34dcd40698999a95faf88ea89
ayoubelhioui <elhioui001@gmail.com>
2025-04-21 17:20:13 +0100

    testing the app

--- src/main/java/org/example/hypertube/HypertubeApplication.java
+++ src/main/java/org/example/hypertube/HypertubeApplication.java
@@ -2,12 +2,19 @@ package org.example.hypertube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class HypertubeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HypertubeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HypertubeApplication.class, args);
+    }
+    @GetMapping("/")
+    public String hello(){
+        return "helloWorld";
+    }
 
 }
